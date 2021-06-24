b857fc3259b8f1dc5e7a75bba990f62c32365c98
BADBOYIII <apisit_xyz2000@yahoo.com>
2021-06-24 16:52:21 +0700

    Service project completed.

--- BlogLab.Web/Startup.cs
+++ BlogLab.Web/Startup.cs
@@ -12,17 +12,17 @@ namespace BlogLab.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {            
+            Configuration = config;
+        }        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+            services.Configure<CloudinaryOptions>(Configuration.GetSection("CloudinaryOptions"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
